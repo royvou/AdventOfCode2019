5b0bd5f1a23cc0ff6b53651f087ffd623af56888
Roy <royvou@hotmail.com>
2019-12-06 22:47:21 +0100

    Solved Day 4

--- Solutions/Year2019/Day04/Solution.cs
+++ Solutions/Year2019/Day04/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -36,8 +37,9 @@ namespace AdventOfCode.Solutions.Year2019
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private bool IsValidNumber(int currentIndex)
-            => HasSameNumbers(currentIndex) &&
-               HasIncreasingNumbers(currentIndex);
+            =>  HasIncreasingNumbers(currentIndex)
+                && HasSameNumbers(currentIndex);
+              
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private bool HasIncreasingNumbers(in int currentIndex)
@@ -82,7 +84,7 @@ namespace AdventOfCode.Solutions.Year2019
             var endIndex = int.Parse(inputs[1]);
 
             var result = CalculateNumbers2(startIndex, endIndex);
-
+    
             return result.Count().ToString();
         }
 
@@ -99,12 +101,72 @@ namespace AdventOfCode.Solutions.Year2019
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private bool IsValidNumber2(in int currentIndex)
-            => HasSameNumbers(currentIndex)
-            && HasIncreasingNumbers(currentIndex)
+            => HasIncreasingNumbers(currentIndex)
+            && HasSameNumbers(currentIndex)
             && HasAtleastGroupOfExactlyTwo(currentIndex);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private bool HasAtleastGroupOfExactlyTwo(int currentIndex) 
+        private bool HasAtleastGroupOfExactlyTwoLINQ(int currentIndex) 
             => currentIndex.ToString().GroupBy(x => x).Any(x => x.Count() == 2);
+        private bool HasAtleastGroupOfExactlyTwoLinqNative(int currentIndex)
+        {
+            var result = false;
+            var array = new int[10];
+            //var array = ArrayPool<int>.Shared.Rent(10); //Array 0-9 for each number count
+
+            int currentIndexLocal = currentIndex;
+            while (currentIndexLocal > 0)
+            {
+                var lastNumber = currentIndexLocal % 10;
+                array[lastNumber]++;
+                currentIndexLocal /= 10;
+            }
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == 2)
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            //ArrayPool<int>.Shared.Return(array, true);
+
+            return result;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool HasAtleastGroupOfExactlyTwo(int currentIndex)
+        {
+            int itemsInGroup = 1;
+            int currentIndexLocal = currentIndex;
+            int lastNumber = -1;
+            while (currentIndexLocal > 0)
+            {
+                var currentNumber = currentIndexLocal % 10;
+
+
+                if (lastNumber == currentNumber)
+                {
+                    itemsInGroup += 1;
+                }
+                else if (lastNumber != currentNumber
+                    && itemsInGroup == 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    itemsInGroup = 1;
+                }
+
+                lastNumber = currentNumber;
+                currentIndexLocal /= 10;
+            }
+            return itemsInGroup == 2;
+        }
     }
 }
