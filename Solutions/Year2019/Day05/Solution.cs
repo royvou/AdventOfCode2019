108868663d7e401f7d8a7b53ac7316037ddf52d2
Roy <royvou@hotmail.com>
2019-12-07 00:03:22 +0100

    Day 5 - Part 1

--- Solutions/Year2019/Day05/Solution.cs
+++ Solutions/Year2019/Day05/Solution.cs
@@ -1,4 +1,7 @@
-﻿namespace AdventOfCode.Solutions.Year2019
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Solutions.Year2019
 {
     public class Day05 : ASolution
     {
@@ -8,12 +11,81 @@
 
         protected override string SolvePartOne()
         {
-            return null;
+            var inputList = Input.ToIntArray(",");
+
+           // var inputList = "1002,4,3,4,33".ToIntArray(",");
+            return SolvePart1With(inputList ,1).ToString();
         }
 
         protected override string SolvePartTwo()
         {
             return null;
+        }
+
+        private static int SolvePart1With(int[] memory, int input)
+        {
+            int currentInput = input;        
+
+            for (var index = 0; index < memory.Length;)
+            {
+                //Only last 2 digits are OpCode
+                var inst = memory[index];
+                var op = (inst % 100 );
+                var opMod = inst / 100;
+
+                Func<int, int> argPos = (int i) =>
+                    memory[memory[index + i]];
+
+                Func<int, int> argDir = (int i) =>
+                    memory[index + i];
+
+                Func<int, ParameterMode> getMode = (int i) =>
+                   (memory[index] / (int)Math.Pow(10, i + 1) % 10) == 0 ? ParameterMode.Position : ParameterMode.Immediate;
+
+                Func<int, int> pos = (int i) =>
+                    getMode(i) == ParameterMode.Immediate
+                    ? argDir(i)
+                    : argPos(i);
+
+                var instructionsDone = 0;
+                switch (op)
+                {
+                    case 1: 
+                       memory[ memory[index + 3]] = pos(1) + pos(2);
+                        instructionsDone = 4;
+                        break;
+                    case 2:
+                       memory[ memory[index + 3]] = pos(1) * pos(2); 
+                        instructionsDone = 4;
+                        break;
+
+                    case 3:
+                      memory[memory[index + 1]] = currentInput;
+                        instructionsDone = 2;
+                        break;
+
+                    case 4:
+                        var result = pos(1);
+                        if (result != 0)
+                            return result;
+
+                        instructionsDone = 2;
+                        break;
+                    case 99:
+                        goto result;
+                        break;
+                }
+              
+                index += instructionsDone;
+            }
+result:
+            return memory[0];
+        }
+
+        public enum ParameterMode
+        {
+            Position,
+            Immediate
         }
     }
 }
