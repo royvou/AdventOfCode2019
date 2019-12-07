f41f4f3b28e17e47db8d3e528cdf2a7d21d344cf
Roy <royvou@hotmail.com>
2019-12-07 21:46:35 +0100

    Solved Day 6

--- Solutions/Year2019/Day06/Solution.cs
+++ Solutions/Year2019/Day06/Solution.cs
@@ -1,4 +1,8 @@
-﻿namespace AdventOfCode.Solutions.Year2019
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Solutions.Year2019
 {
     public class Day06 : ASolution
     {
@@ -8,12 +12,91 @@
 
         protected override string SolvePartOne()
         {
+            var map = CreateMap(Input);
+
+            return StartRecursive(map, "COM").ToString();
+
+        }
+
+        private Dictionary<string, IList<string>> CreateMap(string input)
+        {
+            var map = new Dictionary<string, IList<string>>();
+
+            foreach (var line in input.SplitByNewline())
+            {
+                var io = line.IndexOf(")");
+                var key = line.Substring(0, io);
+                var value = line.Substring(io + 1);
+
+                if (!map.ContainsKey(key))
+                    map[key] = new List<string>();
+
+                map[key].Add(value);
+            }
+
+            return map;
+        }
+
+        private int StartRecursive(Dictionary<string, IList<string>> map, string currentItem, int score = 0)
+        {
+            var returnScore = score;
+            if (map.TryGetValue(currentItem, out var item) && item.Count > 0)
+            {
+                for (int i = 0; i < item.Count; i++)
+                    returnScore += StartRecursive(map, item[i], score + 1);
+            }
+
+            return returnScore;
+        }
+
+
+
+        protected override string SolvePartTwo()
+        {     
+            var map = CreateMapTwo(Input);
+
+            var par1 = GetParentsFor(map, "SAN").ToList();
+            var par2 = GetParentsFor(map, "YOU").ToList();
+
+            for(int i =0; i < par1.Count;i++)
+                if (par2.Contains(par1[i]))
+                {
+                    return (i + par2.IndexOf(par1[i])).ToString();
+                }
+
             return null;
         }
 
-        protected override string SolvePartTwo()
+        private IEnumerable<string> GetParentsFor(Dictionary<string, IList<string>> map, string v)
         {
-            return null;
+            if (map.ContainsKey(v))
+            {
+                foreach(var item in map[v])
+                {
+                    yield return item;
+
+                    foreach (var item2 in GetParentsFor(map, item))
+                        yield return item2;
+                }
+            }        
+        }
+
+        private Dictionary<string, IList<string>> CreateMapTwo(string input)
+        {
+            var map = new Dictionary<string, IList<string>>();
+
+            foreach (var line in input.SplitByNewline())
+            {
+                var io = line.IndexOf(")");
+                var key = line.Substring(0, io).Trim();
+                var value = line.Substring(io + 1).Trim();
+                if (!map.ContainsKey(value))
+                    map[value] = new List<string>();
+
+                map[value].Add(key);
+            }
+
+            return map;
         }
     }
 }
